7c4923014611ef9afea719d565e6648b3dc66ba9
Nada Ashraf <nadaashraf11@icloud.com>
2019-11-05 00:05:52 +0200

    Binned WBC attribute

--- model.R
+++ model.R
@@ -2,3 +2,9 @@
 hcv_data <- read.csv("./dataset/HCV-Egy-Data.csv")
 summary(hcv_data)
 head(hcv_data)
+
+# Discretization
+# WBC
+library(dplyr)
+hcv_dis<- mutate (hcv_data,WBC = ifelse (WBC <= 4000, 0, 
+                  ifelse(WBC > 4000 & WBC <= 11000, 1,2)))
