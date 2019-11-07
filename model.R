2fbc1c9f8951599181b26e664dfab098258bd380
Nada Ashraf <nadaashraf11@icloud.com>
2019-11-07 12:07:04 +0200

    Dataframe sent to discretize function dynamically

--- model.R
+++ model.R
@@ -11,17 +11,19 @@ library(rlang) # for sym()
 
 # Discretization
 
-discretize <- function(feature,column_name,A,B) {
+discretize <- function(data, column_name, A, B) {
   column_sym <- sym(column_name)
   hcv_dis <<- hcv_dis %>% mutate(!!column_sym :=
-                                     cut(
-                                       feature,
+                                   cut(data[[column_name]],
                                        breaks = c(A),
-                                       labels = c(B)
-                                     ))
+                                       labels = c(B)))
   return(hcv_dis)
 }
 
 
-discretize(hcv_data$WBC,"WBC",c(0, 4000, 11000, 12101),c(1, 2, 3))
-discretize(hcv_data$Age,"Age",c(0, 32, 37, 42,47,52,57,62),c(1, 2, 3,4,5,6,7))
+discretize(hcv_data, "WBC", c(0, 4000, 11000, 12101), c(1, 2, 3))
+discretize(hcv_data,
+           "Age",
+           c(0, 32, 37, 42, 47, 52, 57, 62),
+           c(1, 2, 3, 4, 5, 6, 7))
+
