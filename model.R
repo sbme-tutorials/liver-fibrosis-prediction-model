066dd01c1a439f9956dca28ea0f192f1fa8708d5
Nada Ashraf <nadaashraf11@icloud.com>
2019-11-07 11:54:22 +0200

    Fixed the overwriting problem

--- model.R
+++ model.R
@@ -8,16 +8,18 @@ library(dplyr)
 library(arules)
 library(rlang) # for sym()
 
+
 # Discretization
 
 discretize <- function(feature,column_name,A,B) {
   column_sym <- sym(column_name)
-  hcv_dis <<- hcv_data %>% mutate(!!column_sym :=
+  hcv_dis <<- hcv_dis %>% mutate(!!column_sym :=
                                      cut(
                                        feature,
                                        breaks = c(A),
                                        labels = c(B)
                                      ))
+  return(hcv_dis)
 }
 
 
