19a5e92c4e7e5349651e10281457a95d5fd0ee68
Nada Ashraf <nadaashraf11@icloud.com>
2019-11-07 12:47:57 +0200

    Discretized 'ALT.after.24.w' feature

--- model.R
+++ model.R
@@ -2,6 +2,7 @@
 hcv_data <- read.csv("./dataset/HCV-Egy-Data.csv")
 summary(hcv_data)
 head(hcv_data)
+colnames(hcv_data)
 
 # Load libraries
 library(dplyr)
@@ -25,7 +26,6 @@ discretize(hcv_data,
            "Age",
            c(0, 32, 37, 42, 47, 52, 57, 62),
            c(1, 2, 3, 4, 5, 6, 7))
-
 discretize(hcv_data, "BMI", c(0, 18.5, 25, 30, 35, 40), c(1, 2, 3, 4, 5))
 discretize(hcv_data, "RBC", c(0, 3000000, 5000000, 5018451), c(1, 2, 3))
 discretize(hcv_data, "Plat", c(93013, 100000, 255000, 226465), c(1, 2, 3))
@@ -35,12 +35,12 @@ discretize(hcv_data, "ALT.12", c(0, 20, 40, 128), c(1, 2, 3))
 discretize(hcv_data, "ALT.24", c(0, 20, 40, 128), c(1, 2, 3))
 discretize(hcv_data, "ALT.36", c(0, 20, 40, 128), c(1, 2, 3))
 discretize(hcv_data, "ALT.48", c(0, 20, 40, 128), c(1, 2, 3))
+discretize(hcv_data, "ALT.after.24.w", c(0, 20, 40, 128), c(1, 2, 3))
 discretize(hcv_data, "RNA.Base", c(0, 5, 1201086), c(1, 2))
 discretize(hcv_data, "RNA.4", c(0, 5, 1201715), c(1, 2))
 discretize(hcv_data, "RNA.12", c(0, 5, 3731527), c(1, 2))
 discretize(hcv_data, "RNA.EOT", c(0, 5, 808450), c(1, 2))
 discretize(hcv_data, "RNA.EF", c(0, 5, 808450), c(1, 2))
-
 if (hcv_data$Gender == 1) {
   discretize(hcv_data, "HGB", c(0, 14, 17.5, 20), c(1, 2, 3))
 } else if (hcv_data$Gender == 2)  {
