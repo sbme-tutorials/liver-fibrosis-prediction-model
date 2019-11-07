43c413dbe8d49804f305a3883d7057b550699a51
Nada Ashraf <nadaashraf11@icloud.com>
2019-11-07 10:13:27 +0200

    Modified discretization function

--- model.R
+++ model.R
@@ -3,8 +3,15 @@ hcv_data <- read.csv("./dataset/HCV-Egy-Data.csv")
 summary(hcv_data)
 head(hcv_data)
 
+# Load libraries
+library(dplyr)
+library(arules)
+
 # Discretization
 # WBC
-library(dplyr)
-hcv_dis<- mutate (hcv_data,WBC = ifelse (WBC <= 4000, 0, 
-                  ifelse(WBC > 4000 & WBC <= 11000, 1,2)))
+hcv_dis <- hcv_data %>% mutate (WBC =
+                                  cut(
+                                    WBC,
+                                    breaks = c(0, 4000, 11000, 12101),
+                                    labels = c(1, 2, 3)
+                                  ))
