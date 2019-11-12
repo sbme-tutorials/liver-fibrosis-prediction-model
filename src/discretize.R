da07110c8752fb53724017b49dca749a4728b77f
Nada Ashraf <nadaashraf11@icloud.com>
2019-11-12 13:29:16 +0200

    fixed discretize

--- src/discretize.R
+++ src/discretize.R
@@ -1,7 +1,7 @@
 hcv_data_dis <- data.table::copy(hcv_data)
 discretize <- function(data, column_name, A, B) {
   column_sym <- rlang::sym(column_name)
-  hcv_data_dis <<- hcv_data_dis %>% dplyr::mutate(!!column_sym :=
+  hcv_data_dis <<- dplyr::mutate(hcv_data_dis,!!column_sym :=
                                              cut(data[[column_name]],
                                                  breaks = c(A),
                                                  labels = c(B)))
