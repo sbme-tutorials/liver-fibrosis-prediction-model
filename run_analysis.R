5ea397cec6b34d4f7c567b192c200dadafd05fc1
Nada Ashraf <nadaashraf11@icloud.com>
2019-12-05 11:40:37 +0200

    sourced new files in run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,16 @@
 # To perform discretization run
 source("./src/discretize.R")
 
-# To fit knn model run
+# To fit knn model with cross validation run
 source("./src/knn.R")
+
+# To fit knn model with stratified cross validation run
+source("./src/stratified_knn.R")
 
 # To fit naive bayes model run
 source("./src/naive_bayes.R")
+
+# To fit naive bayes model with stratified cross validation run
+source("./src/stratified_naive.R")
+
+
